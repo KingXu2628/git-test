470c593844b80cc127de80d4bb0dd0c2ed1bf74d
KingXu2628 <120448103+KingXu2628@users.noreply.github.com>
2022-12-15 11:45:43 +0800

    pull commit

--- src/main/java/com/king/git/GitTest.java
+++ src/main/java/com/king/git/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git42222 push");
+         System.out.println("hello git333 push");
+        
     }
 }
